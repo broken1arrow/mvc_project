749c2c4a4c1f7b68bbeda867d61fa2abd761c527
broken1arrow <broken2arrow1@gmail.com>
2025-05-22 19:35:56 +0200

    Change the page logic
    It now using several classes.

---
+++ src/Controller/ControllerApi.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Books;
+use App\game\CardsUtility;
+use App\cards\CardsShuffleGame;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+
+class ControllerApi extends AbstractController
+{
+
+    private CardsUtility $cardsUtility;
+
+
+
+    public function getParameter(string $name): array|bool|string|int|float|\UnitEnum|null
+    {
+        return parent::getParameter($name);
+    }
+
+
+    #[Route('/api', name: 'api')]
+    public function api(): Response
+    {
+        $some = [
+            "api" => "api",
+            "about" => "about",
+            "report" => "report",
+            "index" => "/",
+            "quote" => "api/quote",
+            "cards" => "cards"
+        ];
+        return $this->render('./page/api.html.twig', [
+            'title' => 'Api',
+            'routes' => json_encode($some,  JSON_UNESCAPED_SLASHES),
+        ]);
+    }
+
+
+    #[Route('/api/quote', name: 'quote')]
+    public function quote(): Response
+    {
+        $datee = date(DATE_RFC2822);
+        $some = [
+            "quote1" => "$datee what goes up must come down.",
+            "quote2" => "$datee ropa inte hej nar du ar over an.",
+            "quote3" => "$datee efter regn kommer solsken.",
+            "quote4" => "$datee man saknar inte kon forran baset ar tomt."
+        ];
+
+        $rand = array_rand($some);
+        /*   return $this->render('./page/api-blank.html.twig', [
+            'title' => 'Quote',
+            'routes' => json_encode($some[$rand], 1),
+        ]);
+        ]); */
+
+        $response = new Response();
+        $response->setContent(json_encode(['Quote' => $some[$rand]], JSON_UNESCAPED_SLASHES));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
+
+    #[Route('/api/game', name: 'game-stats', methods: ['GET', 'POST'])]
+    public function gameStats(Request $request, SessionInterface $session): Response
+    {
+        $cardsUtility = $this->cardsUtility = new CardsUtility($session);
+        $toJson[] = null;
+        if ($cardsUtility->getPlayerData() != null) {
+            foreach ($cardsUtility->getPlayerData() as $value) {
+                if ($value != null)
+                    $toJson[] = ["{$value->getHtmlData()}  " => "{$value->getAmount()}"];
+            }
+        } else {
+            $toJson = ['data' => 'no data set'];
+        }
+        echo json_encode($toJson,        JSON_UNESCAPED_SLASHES);
+        return $this->render('./page/api-blank.html.twig', [
+            'title' => 'Quote',
+            'routes' => null,
+        ]);
+    }
+
+    #[Route('/api/deck', name: 'deck-stats', methods: ['GET', 'POST'])]
+    public function deckStats(Request $request, SessionInterface $session): Response
+    {
+        $cards = $session->get("cards");
+        $toJson[] = [];
+        if ($cards != null) {
+            $index = 0;
+            foreach ($cards as $value) {
+                if ($value != null) {
+                    $toJson[] = ["{$index}" => "{$value}"];
+                    $index++;
+                }
+            }
+        } else {
+            $toJson = ['data' => 'no data set'];
+        }
+        $response = new Response();
+        $response->setContent(json_encode($toJson, 0));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
+
+    #[Route('/cards', name: 'cards', methods: ['GET', 'POST'])]
+    public function cards(Request $request, SessionInterface $session): Response
+    {
+        $cardsShuffleGame = new CardsShuffleGame($request, $session);
+        $cards =  $cardsShuffleGame->handleCards();
+
+        return $this->render('/page/cards.html.twig', [
+            'title' => 'Cards',
+            'cards' => $cards ?? "<div class=\"card_item\" style=\"grid-column: 1/-1;\">You must click on reset to set new cards.</div>",
+            'sub_title' => "Deck options:"
+        ]);
+    }
+
+    #[Route('api/library/books', name: 'books-list', methods: ['GET', 'POST'])]
+    public function booksList(Request $request, SessionInterface $session, ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        $bookData = $entityManager->getRepository(Books::class)->findAll();
+        $books = [];
+        foreach ($bookData as $data) {
+            $books[] = [$data->getIsbn() => $data->__toString()];
+        }
+
+        $response = new Response();
+        $response->setContent(json_encode($books, 0));
+
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
+
+
+    #[Route('api/library/book/<isbn>', name: 'one-book', methods: ['GET', 'POST'])]
+    public function book(Request $request, SessionInterface $session, ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $bookid =  $request->query->get("bookid");
+        $bookData = $entityManager->getRepository(Books::class)->findOneBy(['isbn' => $bookid]);
+
+        $response = new Response();
+        if ($bookData)
+            $response->setContent(json_encode([$bookData->getIsbn() =>  $bookData->__toString()], 0));
+        else
+            $response->setContent(json_encode(["blank" =>  "Found nothing"], 0));
+
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
+}
---
+++ src/Controller/ControllerCard.php
@@ -0,0 +1,139 @@
+<?php
+namespace App\Controller;
+
+use App\cards\CardsShuffleGame;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class ControllerCard  extends AbstractController
+{
+    #[Route('/card', name: 'card', methods: ['GET', 'POST'])]
+    public function card(Request $request, SessionInterface $session): Response
+    {
+        $cardsShuffleGame = new CardsShuffleGame($request, $session);
+        $cards =  $cardsShuffleGame->handleCards();
+        return $this->render('/page/cards.html.twig', [
+            'title' => 'Cards',
+            'cards' => $cards ?? "<div class=\"card_item\" style=\"grid-column: 1/-1;\">You must click on reset to set new cards.</div>",
+            'sub_title' => "Deck options:"
+        ]);
+    }
+
+
+    #[Route('/cards/deck/shuffle', name: 'shuffle', methods: ['GET', 'POST'])]
+    public function shuffle(Request $request, SessionInterface $session): Response
+    {
+        $cards = null;
+        $cardsList = $session->get("cards");
+
+        shuffle($cardsList);
+        $cards = implode("", array_values($cardsList));
+
+        return $this->render('./page/cards.html.twig', [
+            'title' => 'Cards',
+            'cards' => $cards ?? null,
+            'sub_title' => "Deck options:"
+        ]);
+    }
+
+    #[Route('/cards/deck', name: 'deck', methods: ['GET', 'POST'])]
+    public function deck(Request $request, SessionInterface $session): Response
+    {
+        $cards = null;
+        $cardsList = $session->get("cards");
+        if ($cardsList != null) {
+            $cards = implode("", array_values($cardsList));
+        } else {
+            $cards = "<div class=\"card_item\" style=\"grid-column: 1/-1;\">You must reset to draw new cards.</div>";
+        }
+
+        return $this->render('./page/cards.html.twig', [
+            'title' => 'Cards',
+            'cards' => $cards ?? null,
+            'sub_title' => "Deck options:"
+        ]);
+    }
+
+
+    #[Route('/cards/deck/draw', name: 'draw', methods: ['GET', 'POST'])]
+    public function draw(Request $request, SessionInterface $session): Response
+    {
+        $cards = null;
+        $deck = $session->get("cards");
+        if ($deck != null) {
+            $index = array_rand($deck);
+            $cards = $deck[$index];
+            unset($deck[$index]);
+            $session->set("cards", $deck);
+        } else {
+            $cards = "<div class=\"card_item\" style=\"grid-column: 1/-1;\">You must reset to draw new cards.</div>";
+        }
+
+        return $this->render('./page/cards.html.twig', [
+            'title' => 'Cards',
+            'cards' => $cards ?? null,
+            'sub_title' => "Deck options:"
+        ]);
+    }
+
+    #[Route('/cards/deck/card', name: 'card-one', methods: ['GET', 'POST'])]
+    public function drawCard(Request $request, SessionInterface $session): Response
+    {
+        $cards = null;
+        $deck = $session->get("cards");
+        if ($deck != null) {
+            $index = array_rand($deck);
+            $cards = $deck[$index];
+            unset($deck[$index]);
+            $session->set("cards", $deck);
+        } else {
+            $cards = "<div class=\"card_item\" style=\"grid-column: 1/-1;\">You must reset to draw new cards.</div>";
+        }
+
+        return $this->render('./page/cards.html.twig', [
+            'title' => 'Cards',
+            'cards' => $cards ?? null,
+            'sub_title' => "Deck options:"
+        ]);
+    }
+
+    #[Route('cards/deck/draw/:number', name: 'draw-amount', methods: ['GET', 'POST'])]
+    public function drawAmount(Request $request, SessionInterface $session): Response
+    {
+        $cards = null;
+        $cardsList = $session->get("cards");
+        if ($cardsList != null) {
+            $cards = implode("", array_values($cardsList));
+        } else {
+            $cards = "<div class=\"card_item\" style=\"grid-column: 1/-1;\">You must reset to draw new cards.</div>";
+        }
+
+        return $this->render('./page/cards.html.twig', [
+            'title' => 'Cards',
+            'cards' => $cards ?? null,
+            'sub_title' => "Deck options:"
+        ]);
+    }
+
+    #[Route('cards/deck/card/:number', name: 'draw-amount-two', methods: ['GET', 'POST'])]
+    public function drawAmountCard(Request $request, SessionInterface $session): Response
+    {
+        $cards = null;
+        $cardsList = $session->get("cards");
+        if ($cardsList != null) {
+            $cards = implode("", array_values($cardsList));
+        } else {
+            $cards = "<div class=\"card_item\" style=\"grid-column: 1/-1;\">You must reset to draw new cards.</div>";
+        }
+
+        return $this->render('./page/cards.html.twig', [
+            'title' => 'Cards',
+            'cards' => $cards ?? null,
+            'sub_title' => "Deck options:"
+        ]);
+    }
+}
